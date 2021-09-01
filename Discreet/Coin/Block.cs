d2afc76d8ee89f06fb933de1ac916dc31e050585
Brandon Koerner <bksquared1024@gmail.com>
2021-09-01 00:40:00 -0400

    add Coin.Block stub and DB.DB stub with notes

---
+++ Discreet/Coin/Block.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace Discreet.Coin
+{
+    /**
+     * This block class is intended for use in testnet.
+     * Blocks will, in the future, also specify position in the DAG.
+     * Global indices will be deterministically generated for the sake of DAG consistency.
+     * The current consensus among developers for amount_output_index and tx_index is generation during the addition of the head block in the DAG.
+     * All transactions in blocks in a round of consensus are added in order of timestamp, ensuring all blocks are processed.
+     * For a blockchain this simplifies to a single block addition.
+     */
+    public class Block
+    {
+        public byte Version;
+        public uint NumOutputs;
+
+        public ulong Timestamp;
+        public ulong Height;
+        public ulong Fee;
+
+        public Cipher.SHA256 PreviousBlock;
+        public Cipher.SHA256 Hash;
+
+        public uint NumTXs;
+        public uint NumUTXOs;
+        public uint BlockSize;
+        public Cipher.SHA256[] Transactions;
+        public uint[] Indices;
+    }
+}
---
+++ Discreet/DB/DB.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Discreet.DB
+{
+    /**
+     * The following DB schema is adopted from Monero, with some simplification.
+     * 
+     * Table            Key             Value
+     * -----            ---             -----
+     * spent_keys       input hash      -
+     * 
+     * tx_pool_meta     tx hash         tx metadata
+     * tx_pool_blob     tx hash         tx blob
+     * 
+     * outputs          output index    tx output
+     * 
+     * tx_indices       tx hash         {tx ID, metadata}
+     * txs              tx ID           tx
+     * 
+     * block_info       block ID        {block metadata}
+     * block_heights    block hash      block height
+     * blocks           block ID        block blob
+     * 
+     * For the sake of efficiency, certain data is duplicated slightly for better retrieval.
+     * Spent keys (I.E. linking tags) are duplicated for quick double spend verification.
+     * TX Outputs are duplicated (save for TransactionSrc) in transactions/outputs tables for quick use in mixin rings.
+     * 
+     * For the sake of clarification, a description of "indices" will be used here.
+     * Wherever the word "index/indices" is used, I have done my best to clarify specifically what this means in each 
+     * context. Usually this will refer to the output index, which is used for quick grabbing of outputs for use in
+     * mixins. This is called the Global Index of the output, and is determined when the head block is added to the 
+     * database. Since we guarantee a topological sort of transactions via consensus, we can deterministically add 
+     * each transaction to the database to get transaction indices and the outputs for each transaction to the 
+     * database consistently to get the global indices for each output. As such, global index and transaction index
+     * information is consistent among all honest nodes (at the cost of availability and partition tolerance); yet,
+     * validation/consensus regains availablility and partition tolerance (CAP theorem shiz). 
+     */
+    public class DB
+    {
+        //TODO: Implement
+    }
+}
