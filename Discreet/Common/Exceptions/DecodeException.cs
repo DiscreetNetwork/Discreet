0926bbf91cc888c03f07d1125720d9b42a8b2110
Brandon Koerner <bksquared1024@gmail.com>
2021-10-06 16:36:47 -0400

    various changes

--- Discreet/Common/Exceptions/DecodeException.cs
+++ Discreet/Common/Exceptions/DecodeException.cs
@@ -4,21 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Discreet.Visor
+namespace Discreet.Common.Exceptions
 {
-    public class DecodeException: Exception
+    public class DecodeException : Exception
     {
         public DecodeException(string msg) : base(msg) { }
 
         public DecodeException(string entryPoint, string type) : base(entryPoint + ": error while decoding " + type) { }
 
         public DecodeException(string entryPoint, string type, string msg) : base(entryPoint + ": error while decoding " + type + ": " + msg) { }
-    }
-
-    public class DatabaseException: Exception
-    {
-        public DatabaseException(string msg) : base(msg) { }
-
-        public DatabaseException(string entryPoint, string msg) : base(entryPoint + ": error while calling database function " + msg) { }
     }
 }
--- Discreet/Visor/Logger.cs
+++ Discreet/Visor/Logger.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Discreet.Visor
 {
@@ -12,6 +13,70 @@ namespace Discreet.Visor
      */
     public class Logger
     {
+        private static Logger logger;
+
+        private static object logger_lock = new object();
+
+        public static Logger GetLogger()
+        {
+            lock (logger_lock)
+            {
+                if (logger == null) Initialize();
+
+                return logger;
+            }
+        }
+
+        public static void Initialize()
+        {
+            lock (logger_lock)
+            {
+                if (logger == null)
+                {
+                    logger = new Logger(VisorConfig.GetDefault().LogPath);
+                }
+            }
+        }
+
+        private string path;
+        private DateTime openLogTime;
+        private StreamWriter openLog;
+        private string openLogPath;
+
+        public Logger(string logpath)
+        {
+            path = logpath;
+
+            if (File.Exists(path)) throw new Exception("Discreet.Visor.Logger: expects a valid directory path, not a file");
+
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            openLogTime = DateTime.Now;
+
+            openLogPath = Path.Combine(path, "log_" + openLogTime.Date.ToLongDateString() + ".txt");
+
+            int i = 0;
+
+            while (File.Exists(openLogPath))
+            {
+                openLogPath = Path.Combine(path, "log_" + openLogTime.Date.ToLongDateString() + $"_{i}.txt");
+            }
+
+            openLog = File.CreateText(openLogPath);
+        }
+
+        public static void Log(string msg)
+        {
+            Logger logger = GetLogger();
+
+            logger.openLog.WriteLine(msg);
+
+            Console.WriteLine(msg);
+        }
+
         // WIP
     }
 }
