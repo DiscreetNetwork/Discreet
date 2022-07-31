0065f03ae898957528e4cdffb2ed99508d3fff0d
ReewassSquared <bksquared1024@gmail.com>
2022-07-31 10:16:00 -0400

    use RNGCryptoServiceProvider for entropy

--- Discreet/Cipher/Randomness.cs
+++ Discreet/Cipher/Randomness.cs
@@ -2,18 +2,24 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Runtime.InteropServices;
+using System.Security.Cryptography;
 
 namespace Discreet.Cipher
 {
     public static class Randomness
     {
-        public static void Random(byte[] data, uint len) => Native.Native.Instance.generate_random_bytes_thread_safe(data, len);
+        private static RNGCryptoServiceProvider csp;
+
+        static Randomness()
+        {
+            csp = new RNGCryptoServiceProvider();
+        }
 
         public static byte[] Random(uint len)
         {
             byte[] bytes = new byte[len];
 
-            Random(bytes, len);
+            csp.GetBytes(bytes);
 
             return bytes;
         }
